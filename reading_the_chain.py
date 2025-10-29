e7c05dcf2a3aa7235d50b86b0fec073fbcff3db0
serafina007 <zeyuqi@seas.upenn.edu>
2025-10-28 20:15:23 -0400

    Update reading_the_chain.py

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -92,7 +92,7 @@ def get_contract_values(contract, admin_address, owner_address):
 	
 	onchain_root = contract.functions.merkleRoot().call()
 	has_role = contract.functions.hasRole(default_admin_role, admin_address).call()
-	prime = contract.functions.getPrimeByOwner(ower_address).call()
+	prime = contract.functions.getPrimeByOwner(owner_address).call()
 	
 	return onchain_root, has_role, prime
 
